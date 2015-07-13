1af663d0a150493b134c4ec14950fbdf560104bf
jokamjohn <johnkagga@gmail.com>
2015-07-13 13:58:58 +0300

    added the insert page and database

---
+++ admin/insert_post.php
@@ -0,0 +1,48 @@
+<html>
+<head>
+    <title>Insert Post</title>
+</head>
+<body>
+
+    <table align="center">
+        <tr>
+            <td align="center"><h1>New Post</h1></td>
+        </tr>
+        
+        <tr>
+            <td>Post Title</td>
+            <td><label>
+                    <input type="text" name="title" />
+                </label></td>
+        </tr>
+
+        <tr>
+            <td>Post Author</td>
+            <td><label>
+                    <input type="text" name="author" />
+                </label></td>
+        </tr>
+
+        <tr>
+            <td>Post Keywords</td>
+            <td><label>
+                    <input type="text" name="keywords" />
+                </label></td>
+        </tr>
+
+        <tr>
+            <td>Post Image</td>
+            <td><label>
+                    <input type="file" name="image" />
+                </label></td>
+        </tr>
+
+        <tr>
+            <td>Post Content</td>
+            <td><label>
+                    <textarea name="content" id="content" cols="30" rows="10"></textarea>
+                </label></td>
+        </tr>
+    </table>
+</body>
+</html>
