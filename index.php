fad62c3971b1300713163a2c5f484d69fa6b1104
jokamjohn <johnkagga@gmail.com>
2015-07-13 09:42:34 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+    <title>CMS Project</title>
+</head>
+
+<!-- Created by John Kagga.-->
+<!-- User: DELL-->
+<!-- Date: 7/13/2015-->
+<!-- Time: 9:33 AM-->
+<body>
+
+</body>
+</html>
