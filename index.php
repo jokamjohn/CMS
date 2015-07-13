ca0a02fffe0659603a23c8e77352ee551231fc68
jokamjohn <johnkagga@gmail.com>
2015-07-13 09:49:38 +0300

    project structure

--- index.php
+++ index.php
@@ -8,6 +8,10 @@
 <!-- Date: 7/13/2015-->
 <!-- Time: 9:33 AM-->
 <body>
-
+<div id="header">Header</div>
+<div id="navbar">Navigation</div>
+<div id="main_content">Content</div>
+<div id="sidebar">Side bar</div>
+<div id="footer">Footer</div>
 </body>
 </html>
