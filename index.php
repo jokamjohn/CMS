be3bda6c6bc150cf54e5a61cefd96c6193d8d7c4
jokamjohn <johnkagga@gmail.com>
2015-07-13 11:32:55 +0300

    styled navbar

--- index.php
+++ index.php
@@ -9,8 +9,24 @@
 <!-- Date: 7/13/2015-->
 <!-- Time: 9:33 AM-->
 <body>
-<div id="header">Header</div>
-<div id="navbar">Navigation</div>
+<div id="header">Header
+    <div id="logo"><img src="#" alt="logo"/></div>
+    <div id="banner"><img src="#" alt="banner"/></div>
+
+</div>
+
+<div id="navbar">
+    <ul id="ul">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Cause</a></li>
+        <li><a href="#">Biography</a></li>
+        <li><a href="#">Photos</a></li>
+        <li><a href="#">About us</a></li>
+        <li><a href="#">Contact</a></li>
+    </ul>
+</div>
+
+
 <div id="main_content">Content</div>
 <div id="sidebar">Side bar</div>
 <div id="footer">Footer</div>
