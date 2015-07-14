a9767588a89e2aa0b7d7c4e3b2d74069991ab6e6
jokamjohn <johnkagga@gmail.com>
2015-07-14 15:18:11 +0300

    admin page design

--- admin/index.php
+++ admin/index.php
@@ -1,11 +1,25 @@
 <html>
     <head>
         <title>Admin Panel</title>
+        <link rel="stylesheet" href="../css/admin_style.css"/>
     </head>
 <body>
 
+<div id="header">
+    <h1>Welcome to the Admin Panel</h1>
+</div>
 
+<div id="sidebar">
+    <h2><a href="#">Logout</a></h2>
+    <h2><a href="#">View Posts</a></h2>
+    <h2><a href="#">Insert New posts</a></h2>
+    <h2><a href="#">View Comments</a></h2>
+</div>
 
+<div id="welcome">
+    <h1>Welcome to the Admin Panel</h1>
+    <p>This is where you manage your blog</p>
+</div>
 
 
 </body>
