b3f9aff579080e91e64a5d763d82a8302a790c2b
jokamjohn <johnkagga@gmail.com>
2015-07-15 13:20:40 +0300

    added a login session

--- admin/index.php
+++ admin/index.php
@@ -1,37 +1,61 @@
+    <?php
+        session_start();
+        if(!isset($_SESSION['user_name'])){
+            /*
+             * Redirect to the login page
+             */
+            header("location: login.php");
+        }
+        else {
+
+
+    ?>
+
+
 <html>
+
     <head>
-        <title>Admin Panel</title>
+
+        <title>
+            Admin Panel
+        </title>
+
         <link rel="stylesheet" href="../css/admin_style.css"/>
+
     </head>
+
 <body>
 
 <!--header-->
-<div id="header">
-    <a href="index.php">
-    <h1>Welcome to the Admin Panel</h1>
-    </a>
-</div>
+    <div id="header">
+        <a href="index.php">
+        <h1>Welcome to the Admin Panel</h1>
+        </a>
+    </div>
 
 <!--sidebar-->
-<div id="sidebar">
-    <h2><a href="#">Logout</a></h2>
-    <h2><a href="../admin/viewpost.php">View Posts</a></h2>
-    <h2><a href="../admin/index.php?id=insert">Insert New posts</a></h2>
-    <h2><a href="#">View Comments</a></h2>
-</div>
+    <div id="sidebar">
+        <h2><a href="#">Logout</a></h2>
+        <h2><a href="../admin/viewpost.php">View Posts</a></h2>
+        <h2><a href="../admin/index.php?id=insert">Insert New posts</a></h2>
+        <h2><a href="#">View Comments</a></h2>
+    </div>
 <!--body-->
-<div id="welcome">
-    <h1>Welcome to the Admin Panel</h1>
-    <p>This is where you manage your blog</p>
+    <div id="welcome">
+        <h1>Welcome to the Admin Panel</h1>
+        <p>This is where you manage your blog</p>
 
-    <?php
-            if(isset($_GET['id'])){
-                include("../admin/insert_post.php");
-            }
-    ?>
-</div>
+        <?php
+                if(isset($_GET['id'])){
+                    include("../admin/insert_post.php");
+                }
+        ?>
+    </div>
 
 <!--body-->
 
 </body>
+
 </html>
+
+    <?php  } ?>
